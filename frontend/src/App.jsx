31a447786942249d5e5aeefc2ea50cb5864f82c1
tlhalesebaeng <tlhalesebaeng@gmail.com>
2024-12-31 17:18:29 +0200

    game design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,56 @@
-import './App.css'
+import "./App.css";
 
-export default function App(){
+const INITIAL_GAME_BOARD = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function App() {
+  const turn = "player x";
+
   return (
-    <>
-    </>
+    <main>
+      <div className="container">
+        <ul className="game-history">
+          <li className="player-x-color">
+            <h2>Player X</h2>
+            <p>{0}</p>
+          </li>
+          <li className="draw-color">
+            <h2>Draw</h2>
+            <p>{0}</p>
+          </li>
+          <li className="player-o-color">
+            <h2>Player O</h2>
+            <p>{0}</p>
+          </li>
+        </ul>
+        <ul className="board-columns">
+          {INITIAL_GAME_BOARD.map((row, rowIndex) => (
+            <li key={rowIndex}>
+              <ul className="board-rows">
+                {row.map((playerSymbol, colIndex) => (
+                  <li key={colIndex}>
+                    {
+                      <button className="btn-player-symbol">
+                        {playerSymbol}
+                      </button>
+                    }
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+        <div
+          className={`player-turn-container ${
+            turn === "player x" ? "player-x-color" : "player-o-color"
+          }`}
+        >
+          <p>{turn === "player x" ? "X" : "O"} turn</p>
+        </div>
+      </div>
+    </main>
   );
 }
