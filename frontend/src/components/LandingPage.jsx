132247569dba8e6b579b22e65b31c2e5d54a674c
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-01-02 07:51:53 +0200

    landing page design

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,28 +1,86 @@
-import "./LandingPage.css";
+import { useState } from 'react';
+import './LandingPage.css';
 
 export default function LandingPage() {
-  return (
-    <main>
-      <div className="container column">
-        <h2 className="landing-page-heading">
-          <span className="X">X</span>
-          <span className="O">O</span>
-        </h2>
-        <div className="player-selection-container column">
-          <h2>Select player 1's mark</h2>
-          <div className="selection-container row">
-            <button className="transparent X">X</button>
-            <button className="selected-player O">0</button>
-          </div>
-          <p>Player X goes first</p>
-          {/*render this when the game type is only player to player */}
-        </div>
-        <div className="game-type column">
-          <button className="player-x-color">Online Multiplayer</button>
-          <button className="draw-color">Single player(vs AI)</button>
-          <button className="player-o-color">Player to Player</button>
-        </div>
-      </div>
-    </main>
-  );
+    //put all this states into one object state
+    const [selectedPlayer, setSelectedPlayer] = useState('player-x');
+    const [selectedGameType, setSelectedGameType] = useState('');
+
+    function handleGameType(gameType) {
+        setSelectedGameType(gameType);
+    }
+
+    function handleSelectedPlayer(player) {
+        setSelectedPlayer(player);
+    }
+
+    return (
+        <main>
+            <div className="container column">
+                <h2 className="landing-page-heading">
+                    <span className="X">X</span>
+                    <span className="O">O</span>
+                </h2>
+                <div className="player-selection-container column">
+                    <h2>Select player 1's mark</h2>
+                    <div className="selection-container row">
+                        <button
+                            onClick={() => handleSelectedPlayer('player-x')}
+                            className={`${
+                                selectedPlayer !== 'player-x'
+                                    ? 'transparent'
+                                    : 'selected-player'
+                            }  X`}
+                        >
+                            X
+                        </button>
+                        <button
+                            onClick={() => handleSelectedPlayer('player-o')}
+                            className={`${
+                                selectedPlayer !== 'player-o'
+                                    ? 'transparent'
+                                    : 'selected-player'
+                            }  O`}
+                        >
+                            0
+                        </button>
+                    </div>
+                    <p>Player X goes first</p>
+                    {/*render this when the game type is only player to player */}
+                </div>
+                <div className="game-type column">
+                    <button
+                        onClick={() => handleGameType('online multiplayer')}
+                        className={`btn-select-game-type player-x-color ${
+                            selectedGameType === 'online multiplayer'
+                                ? 'selected-type'
+                                : null
+                        }`}
+                    >
+                        Online Multiplayer
+                    </button>
+                    <button
+                        onClick={() => handleGameType('single player')}
+                        className={`btn-select-game-type draw-color ${
+                            selectedGameType === 'single player'
+                                ? 'selected-type'
+                                : null
+                        }`}
+                    >
+                        Single player(vs AI)
+                    </button>
+                    <button
+                        onClick={() => handleGameType('player to player')}
+                        className={`btn-select-game-type player-o-color ${
+                            selectedGameType === 'player to player'
+                                ? 'selected-type'
+                                : null
+                        }`}
+                    >
+                        Player to Player
+                    </button>
+                </div>
+            </div>
+        </main>
+    );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import LandingPage from "./components/LandingPage.jsx";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import LandingPage from './components/LandingPage.jsx';
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    {/* <App /> */}
-    <LandingPage />
-  </StrictMode>
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        {/* <App /> */}
+        <LandingPage />
+    </StrictMode>
 );
