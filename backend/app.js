74d0f6e9b539241e641782db3898a081c72bb785
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-01-25 10:15:17 +0200

    deployment fixes

--- backend/app.js
+++ backend/app.js
@@ -88,6 +88,7 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
     console.log('Listening on port 3000...');
 });
