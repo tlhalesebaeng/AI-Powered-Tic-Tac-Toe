8648e72c8a1a91a144dc838defc4c517c10663e1
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-02-04 09:04:55 +0200

    winner bug fix

--- frontend/src/components/ResultModal.jsx
+++ frontend/src/components/ResultModal.jsx
@@ -2,17 +2,19 @@ import { forwardRef, useContext, useEffect } from 'react';
 import './ResultModal.css';
 import Modal from './Modal';
 import { DetailsContext } from '../store/details-context';
+import { GameContext } from '../store/game-context';
 
 const ResultModal = forwardRef(function ResultModal(
     { onHome, onReplay, result },
     ref
 ) {
     const { details } = useContext(DetailsContext);
+    const { gameType } = useContext(GameContext);
 
     const winner = result === 'X' ? 'player-x' : 'player-o';
     let winnerValue;
 
-    if (details.userSymbol === winner) {
+    if (details.userSymbol === winner && gameType !== 'player to player') {
         winnerValue = 'You win';
     } else {
         winnerValue = winner === 'player-x' ? 'Player X wins' : 'Player O wins';
