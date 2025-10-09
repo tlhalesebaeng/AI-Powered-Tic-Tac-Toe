94f66bc23cc3c776103f9693108be974e1a2c425
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-09 10:14:23 +0200

    Remove unnecessary imports

--- frontend/src/pages/RootLayout.jsx
+++ frontend/src/pages/RootLayout.jsx
@@ -1,5 +1,4 @@
 import { Outlet } from 'react-router-dom';
-import './RootLayout.css';
 import GameContextProvider from '../store/game-context';
 import DetailsContextProvider from '../store/details-context';
 
