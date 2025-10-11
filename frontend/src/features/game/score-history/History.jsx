37de805ba1c9e75485021f1ba6dc6fef24014915
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-11 20:05:19 +0200

    Default the history scores to 0

--- frontend/src/features/game/score-history/History.jsx
+++ frontend/src/features/game/score-history/History.jsx
@@ -5,15 +5,15 @@ export default function GameHistory({ history = {} }) {
         <ul className="game-history">
             <li className="player-x-color">
                 <h2>Player X</h2>
-                <p>{history.X}</p>
+                <p>{history.X || 0}</p>
             </li>
             <li className="draw-color">
                 <h2>Draw</h2>
-                <p>{history.draw}</p>
+                <p>{history.draw || 0}</p>
             </li>
             <li className="player-o-color">
                 <h2>Player O</h2>
-                <p>{history.O}</p>
+                <p>{history.O || 0}</p>
             </li>
         </ul>
     );
