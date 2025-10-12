0c9ed921e31c27a63a0280567cc295529a203133
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-12 18:09:23 +0200

    Separate routes configuration and the actual route creation

--- frontend/src/root/App.jsx
+++ frontend/src/root/App.jsx
@@ -1,50 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import routesConfig from './RoutesConfig.jsx';
 
-import GamePage from '../features/game/GamePage.jsx';
-import CreateUsername from '../features/create-username/CreateUsername.jsx';
-import RootLayout from './RootLayout.jsx';
-import ErrorPage from '../features/errors/ErrorPage.jsx';
-import TypesSelection from '../features/select-types/TypesSelection.jsx';
-import CreateSession from '../features/create-session/CreateSession.jsx';
-import DifficultySelection from '../features/select-difficulty/DifficultySelection.jsx';
-
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <RootLayout />,
-        errorElement: <ErrorPage />,
-        children: [
-            {
-                index: true,
-                element: <CreateUsername />,
-            },
-            {
-                path: '/type',
-                element: <TypesSelection />,
-            },
-            {
-                path: '/game',
-                element: <GamePage />,
-            },
-            {
-                path: '/game/:roomId',
-                element: <GamePage />,
-            },
-            {
-                path: '/type/online',
-                element: <CreateSession />,
-            },
-            {
-                path: '/type/ai',
-                element: <DifficultySelection />,
-            },
-            {
-                path: '/type/ai/:difficulty',
-                element: <GamePage />,
-            },
-        ],
-    },
-]);
+const router = createBrowserRouter(routesConfig);
 
 export default function App() {
     return <RouterProvider router={router} />;
---
+++ frontend/src/root/RoutesConfig.jsx
@@ -0,0 +1,47 @@
+import GamePage from '../features/game/GamePage.jsx';
+import CreateUsername from '../features/create-username/CreateUsername.jsx';
+import RootLayout from './RootLayout.jsx';
+import ErrorPage from '../features/errors/ErrorPage.jsx';
+import TypesSelection from '../features/select-types/TypesSelection.jsx';
+import CreateSession from '../features/create-session/CreateSession.jsx';
+import DifficultySelection from '../features/select-difficulty/DifficultySelection.jsx';
+
+const routesConfig = [
+    {
+        path: '/',
+        element: <RootLayout />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                index: true,
+                element: <CreateUsername />,
+            },
+            {
+                path: '/type',
+                element: <TypesSelection />,
+            },
+            {
+                path: '/game',
+                element: <GamePage />,
+            },
+            {
+                path: '/game/:roomId',
+                element: <GamePage />,
+            },
+            {
+                path: '/type/online',
+                element: <CreateSession />,
+            },
+            {
+                path: '/type/ai',
+                element: <DifficultySelection />,
+            },
+            {
+                path: '/type/ai/:difficulty',
+                element: <GamePage />,
+            },
+        ],
+    },
+];
+
+export default routesConfig;
